b1d91dae3d19e6c33f53cab299087ca76afa0a64
NareshModina <nareshmodina@outlook.com>
2022-08-17 22:26:06 +0200

    Created a basic ML project
    added the libraries necessary to execute ML algorithms

---
+++ Basic_ML_practice1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+np.set_printoptions(precision=2)
