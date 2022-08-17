7837c8e170eb12a072cc84ec25431e33b435ba2f
NareshModina <nareshmodina@outlook.com>
2022-08-17 22:32:00 +0200

    add plot data
    added code for plotting 2d and 3d plot

--- Basic_ML_practice1.py
+++ Basic_ML_practice1.py
@@ -7,3 +7,22 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 np.set_printoptions(precision=2)
+#loading data
+X, y = load_iris(return_X_y=True)
+#plotting 2d plot (useful if there are only two features)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+plt.xlabel("Sepal length")
+plt.ylabel("Sepal width")
+#plotting 3d plot using pca to extract important dimensions
+X_reduced = PCA(n_components=3).fit_transform(X)
+fig = plt.figure(1, figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
+ax.scatter(
+    X_reduced[:, 0],
+    X_reduced[:, 1],
+    X_reduced[:, 2],
+    c=y,
+    cmap=plt.cm.Set1,
+    edgecolor="k",
+    s=40,
+)
